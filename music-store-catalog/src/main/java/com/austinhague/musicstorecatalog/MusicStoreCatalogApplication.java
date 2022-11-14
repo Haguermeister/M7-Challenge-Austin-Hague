8556a8713771dd80386a082887653ae8094c82e8
Haguermeister <cadethague70@gmail.com>
2022-11-14 08:35:47 -0500

    Final push

--- music-store-catalog/src/main/java/com/austinhague/musicstorecatalog/MusicStoreCatalogApplication.java
+++ music-store-catalog/src/main/java/com/austinhague/musicstorecatalog/MusicStoreCatalogApplication.java
@@ -1,4 +1,4 @@
-package com.austinhague.app.musicstorecatalog;
+package com.austinhague.musicstorecatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
